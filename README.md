b43902b2daef94232c07a1ead42dcc6fcac8ebbc
Jeffrey Doto <jeffdoto@amazon.com>
2021-03-09 15:01:44 -0500

    Initial checkin

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # sesEmailLambda
+
+
+https://docs.aws.amazon.com/lambda/latest/dg/images-create.html
+
+https://docs.aws.amazon.com/lambda/latest/dg/images-test.html
+
+https://docs.aws.amazon.com/lambda/latest/dg/python-image.html
+
+
+Note: for the purposes of demonstration, I manually created an SES address valid only for my personal email.
+
+https://docs.aws.amazon.com/ses/latest/DeveloperGuide/send-email-getting-started.html
+
+
+A full setup would require some additional time, though this might make it easier:
+
+https://binx.io/blog/2019/11/14/how-to-deploy-aws-ses-domain-identities-dkim-records-using-cloudformation/
+
+
+### Testing
+
+    docker build . -t ses-email-handler:latest
+
+    # default region is used by boto to configure step client
+    docker run -e AWS_DEFAULT_REGION=us-east-1 -e AWS_REGION=us-east-1 -e AWS_ACCESS_KEY_ID=<key_id> \
+        -e AWS_SECRET_ACCESS_KEY=<key> -p 9000:8080  ses-email-handler:latest
+
+    # test with empty post body:
+    curl -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{}'
+
+    # or post with data from json file:
+    curl -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" -d '@sns_event_message.json'
+
+### Pushing to ECR
+
+    aws ecr get-login-password --region us-east-1 --profile=jdoto-ab3 \
+    | docker login --username AWS --password-stdin 483158796244.dkr.ecr.us-east-1.amazonaws.com
+
+    docker tag ses-email-handler 483158796244.dkr.ecr.us-east-1.amazonaws.com/ses-email-handler:1.0.0
+
+    docker push 483158796244.dkr.ecr.us-east-1.amazonaws.com/ses-email-handler:1.0.0
+
+
---
+++ app.py
@@ -0,0 +1,96 @@
+import json
+
+import boto3
+from botocore.exceptions import ClientError
+
+
+def send_email(event):
+
+    job_status = event['detail']['status']
+    job_name = event['detail']['jobName']
+
+    # Replace sender@example.com with your "From" address.
+    # This address must be verified with Amazon SES.
+    SENDER = "Jeffrey Doto <jeffdoto@amazon.com>"
+
+    # Replace recipient@example.com with a "To" address. If your account
+    # is still in the sandbox, this address must be verified.
+    RECIPIENT = "jeffdoto@amazon.com"
+
+    # Specify a configuration set. If you do not want to use a configuration
+    # set, comment the following variable, and the
+    # ConfigurationSetName=CONFIGURATION_SET argument below.
+    #CONFIGURATION_SET = "ConfigSet"
+
+    # If necessary, replace us-west-2 with the AWS Region you're using for Amazon SES.
+    AWS_REGION = "us-east-1"
+
+    # The subject line for the email.
+    SUBJECT = "intSiteCaller Job %s has %s" % (job_name, job_status)
+
+    # The email body for recipients with non-HTML email clients.
+    BODY_TEXT = ("Amazon SES Test (Python)\r\n"
+                 "This email was sent with Amazon SES using the "
+                 "AWS SDK for Python (Boto)."
+                 )
+
+    # The HTML body of the email.
+    BODY_HTML = """<html>
+    <head></head>
+    <body>
+      <h1>Amazon SES Test (SDK for Python)</h1>
+      <p>This email was sent with
+        <a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the
+        <a href='https://aws.amazon.com/sdk-for-python/'>
+          AWS SDK for Python (Boto)</a>.</p>
+    </body>
+    </html>
+                """
+
+    # The character encoding for the email.
+    CHARSET = "UTF-8"
+
+    # Create a new SES resource and specify a region.
+    client = boto3.client('ses',region_name=AWS_REGION)
+
+    # Try to send the email.
+    try:
+        #Provide the contents of the email.
+        response = client.send_email(
+            Destination={
+                'ToAddresses': [
+                    RECIPIENT,
+                ],
+            },
+            Message={
+                'Body': {
+                    'Html': {
+                        'Charset': CHARSET,
+                        'Data': BODY_HTML,
+                    },
+                    'Text': {
+                        'Charset': CHARSET,
+                        'Data': BODY_TEXT,
+                    },
+                },
+                'Subject': {
+                    'Charset': CHARSET,
+                    'Data': SUBJECT,
+                },
+            },
+            Source=SENDER,
+            # If you are not using a configuration set, comment or delete the
+            # following line
+#            ConfigurationSetName=CONFIGURATION_SET,
+        )
+    # Display an error if something goes wrong.
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+    else:
+        print("Email sent! Message ID:"),
+        print(response['MessageId'])
+
+def lambda_handler(event, context):
+    print("Received event: " + json.dumps(event, indent=2))
+
+    send_email(event)
