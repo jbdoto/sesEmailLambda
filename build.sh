634d944fd6f9cda6ac0a1768105d96f38a03a855
Jeffrey Doto <jeffdoto@amazon.com>
2021-03-10 09:42:33 -0500

    Improve build.sh to publish new version

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+TAG=1.0.0
+IMAGE_NAME=ses-email-handler
+ACCOUNT_URI=483158796244.dkr.ecr.us-east-1.amazonaws.com
+REPO_URI=$ACCOUNT_URI/$IMAGE_NAME
+IMAGE_URI=$REPO_URI:$TAG
 
-aws ecr get-login-password --region us-east-1 --profile=jdoto-ab3 | docker login --username AWS --password-stdin 483158796244.dkr.ecr.us-east-1.amazonaws.com
-docker build . -t ses-email-handler
-docker tag ses-email-handler 483158796244.dkr.ecr.us-east-1.amazonaws.com/ses-email-handler:1.0.0
-docker push 483158796244.dkr.ecr.us-east-1.amazonaws.com/ses-email-handler:1.0.0
+docker build . -t $IMAGE_NAME
+docker tag $IMAGE_NAME $IMAGE_URI
+aws ecr get-login-password --region us-east-1 --profile=jdoto-ab3 | docker login --username AWS --password-stdin $ACCOUNT_URI
+docker push $IMAGE_URI
+aws lambda  update-function-code --function-name ses-email-handler --image-uri $IMAGE_URI --publish --profile=jdoto-ab3 --region=us-east-1
