5aca044d945487ec97994e3b7c9486740e75732d
Jeffrey Doto <jeffdoto@amazon.com>
2021-03-10 09:47:34 -0500

    Improve build.sh to publish new version

--- build.sh
+++ build.sh
@@ -9,4 +9,4 @@ docker build . -t $IMAGE_NAME
 docker tag $IMAGE_NAME $IMAGE_URI
 aws ecr get-login-password --region us-east-1 --profile=jdoto-ab3 | docker login --username AWS --password-stdin $ACCOUNT_URI
 docker push $IMAGE_URI
-aws lambda  update-function-code --function-name ses-email-handler --image-uri $IMAGE_URI --publish --profile=jdoto-ab3 --region=us-east-1
+aws lambda  update-function-code --function-name $IMAGE_NAME --image-uri $IMAGE_URI --publish --profile=jdoto-ab3 --region=us-east-1
