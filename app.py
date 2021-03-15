16d28ae7325ef408e3629f40bf3ee19b61e5e62f
Jeffrey Doto <jeffdoto@amazon.com>
2021-03-15 11:23:40 -0400

    Includes time duration in email body

--- app.py
+++ app.py
@@ -3,12 +3,24 @@ import os
 from json2html import *
 import boto3
 from botocore.exceptions import ClientError
-
+import time
+from datetime import timedelta
 
 def send_email(event):
     job_status = event['detail']['status']
     job_name = event['detail']['jobName']
     job_id = event['detail']['jobId']
+    job_create_time = event['detail']['createdAt']
+    job_start_time = event['detail']['startedAt']
+    job_end_time = event['detail']['stoppedAt']
+    job_elapsed_time = job_end_time - job_start_time
+
+    print("Got values: %s, %s, %s, %s", job_create_time, job_start_time, job_end_time, job_elapsed_time)
+
+    job_create_time_hr = time.strftime("%Y-%m-%d %I:%M:%S", time.localtime(job_create_time / 1000))
+    job_start_time_hr = time.strftime("%Y-%m-%d %I:%M:%S", time.localtime(job_start_time / 1000))
+    job_end_time_hr = time.strftime("%Y-%m-%d %I:%M:%S", time.localtime(job_end_time / 1000))
+    job_elapsed_time_hr = str(timedelta(seconds=(job_elapsed_time / 1000)))
 
     # Replace sender@example.com with your "From" address.
     # This address must be verified with Amazon SES.
@@ -32,7 +44,7 @@ def send_email(event):
     # The email body for recipients with non-HTML email clients.
     BODY_TEXT = ("Bushman Lab IntSiteCaller Batch Job Report\r\n")
 
-    table = json2html.convert(json = event)
+    table = json2html.convert(json=event)
 
     # The HTML body of the email.
     BODY_HTML = """<html>
@@ -40,13 +52,17 @@ def send_email(event):
     <body>
       <h1>Bushman Lab IntSiteCaller Batch Job Report</h1>
       <br/>
+      <p> Job Took: %s </p>
+      <p> Job Create Time: %s </p>
+      <p> Job Start Time: %s </p>
+      <p> Job End Time: %s </p>
       <a href='https://console.aws.amazon.com/batch/v2/home?region=us-east-1#jobs/detail/%s'>View job details in console.</a>
       <br/>
       <br/>
       <div>%s</div>
     </body>
     </html>
-                """ % (job_id, table)
+                """ % (job_elapsed_time_hr, job_create_time_hr, job_start_time_hr, job_end_time_hr, job_id, table)
 
     # The character encoding for the email.
     CHARSET = "UTF-8"
@@ -90,6 +106,7 @@ def send_email(event):
     else:
         print("Email sent! Message ID:"),
         print(response['MessageId'])
+
 
 def lambda_handler(event, context):
     print("Received event: " + json.dumps(event, indent=2))
